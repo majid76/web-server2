87c279ad8f4ac1dec860caac794e75c4bffdd55b
Majid Dagha <majid.dagha@gmail.com>
2016-08-28 12:37:20 +1200

    Initialize Repo

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express'); 
+var app = express();
+var PORT = 3000;
+var middleWare = {
+
+	requireAuthentication : function(req,res,next){
+
+		console.log("private route hit");
+		next();
+	},
+	logger: function(req,res,next){
+		// var currentDate = new Date().toString();
+		console.log(req.hostname+' '+req.method+' '+req.url +' '+new Date().toString());
+		next();
+	}
+
+};
+
+app.use(middleWare.logger);
+
+
+app.get('/',function(req,res){
+	res.send('Hello Express!:');
+
+
+});
+
+app.get('/about',middleWare.requireAuthentication,function(req,res){
+	res.send('About Us!');
+
+});
+
+
+// console.log(__dirname);
+app.use(express.static(__dirname+'/public'));
+
+app.listen(PORT,function(){
+
+	console.log('Express server started on PORT: !'+PORT);
+});
